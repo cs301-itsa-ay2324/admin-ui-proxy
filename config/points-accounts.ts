6d7e54aaf79e8e6504cc5317bb20cd047f82d7de
Jerome <68373112+jeromeandrewong@users.noreply.github.com>
2023-10-22 00:57:25 +0800

    fix: convert csv data to json (#1)
    * fix: convert data to json
    
    * chore: remove logs

---
+++ config/points-accounts.ts
@@ -0,0 +1,52 @@
+export const pointsAccountData = [
+  {
+    id: "a73bab06-1baf-4605-a79c-54a43603c0d3",
+    user_id: "da7da4ff-f10c-4b89-ab64-ea7263f6b624",
+    balance: 5130,
+  },
+  {
+    id: "6062f766-5313-4521-9c56-954185b85362",
+    user_id: "8c874087-9f1a-4c12-a4dc-4a4e53282b8e",
+    balance: 9860,
+  },
+  {
+    id: "0617e6c3-11ee-4429-909b-5d30ac65987e",
+    user_id: "8cecd1af-6c38-4186-9fe7-ba1cf15a7379",
+    balance: 7977,
+  },
+  {
+    id: "08f57648-0761-4877-a889-d8842695e1ad",
+    user_id: "11082f02-d942-4ede-893c-0f75f36d4388",
+    balance: 3468,
+  },
+  {
+    id: "9f7073b8-c2a6-4aff-9446-f4b629e4085a",
+    user_id: "718b5985-6df4-4367-aff0-edc1bd90d66e",
+    balance: 6502,
+  },
+  {
+    id: "69353168-a74a-49a1-964b-7afe981886ce",
+    user_id: "7e92ce5f-60d3-4224-b4b2-c9b29e73de16",
+    balance: 7075,
+  },
+  {
+    id: "370ae024-588d-4d8a-bf1b-c8679f22f930",
+    user_id: "4d8535e2-0e73-4e8d-8014-cf5c59df2d30",
+    balance: 7142,
+  },
+  {
+    id: "23993893-1922-4253-bc8a-5ca8004ee28b",
+    user_id: "2172de88-9b32-4321-9461-8b8a5efeb7e9",
+    balance: 5258,
+  },
+  {
+    id: "43871cb4-d40a-48e2-bb55-df919223b215",
+    user_id: "11dea91f-e5d5-4784-8f71-59c202a168cf",
+    balance: 876,
+  },
+  {
+    id: "6446c5e9-c228-4cdb-867c-2ae4fc929ed5",
+    user_id: "4476e275-2d9e-43f4-8bf2-58c51b93ab84",
+    balance: 3299,
+  },
+]
---
+++ config/users.ts
@@ -0,0 +1,62 @@
+export const usersData = [
+  {
+    id: "da7da4ff-f10c-4b89-ab64-ea7263f6b624",
+    email: "waltraud_ondricka@oreilly.org",
+    first_name: "Waltraud",
+    last_name: "Ondricka",
+  },
+  {
+    id: "8c874087-9f1a-4c12-a4dc-4a4e53282b8e",
+    email: "latricia_schulist@abbott.com",
+    first_name: "Latricia",
+    last_name: "Schulist",
+  },
+  {
+    id: "8cecd1af-6c38-4186-9fe7-ba1cf15a7379",
+    email: "russel.stephan@kihn.name",
+    first_name: "Stephan",
+    last_name: "Russel",
+  },
+  {
+    id: "11082f02-d942-4ede-893c-0f75f36d4388",
+    email: "ryan_schuster@oberbrunner-sauer.name",
+    first_name: "Ryan",
+    last_name: "Schuster",
+  },
+  {
+    id: "718b5985-6df4-4367-aff0-edc1bd90d66e",
+    email: "bobbie.mitchell@ward.org",
+    first_name: "Bobbie",
+    last_name: "Mitchell",
+  },
+  {
+    id: "7e92ce5f-60d3-4224-b4b2-c9b29e73de16",
+    email: "reatha_becker@simonis.info",
+    first_name: "Reatha",
+    last_name: "Becker",
+  },
+  {
+    id: "4d8535e2-0e73-4e8d-8014-cf5c59df2d30",
+    email: "leopoldo.borer@kunze-dach.co",
+    first_name: "Leopoldo",
+    last_name: "Borer",
+  },
+  {
+    id: "2172de88-9b32-4321-9461-8b8a5efeb7e9",
+    email: "catherina_kiehn@watsica.info",
+    first_name: "Catherina",
+    last_name: "Kiehn",
+  },
+  {
+    id: "11dea91f-e5d5-4784-8f71-59c202a168cf",
+    email: "frankie_roberts@prohaska.io",
+    first_name: "Frankie",
+    last_name: "Roberts",
+  },
+  {
+    id: "4476e275-2d9e-43f4-8bf2-58c51b93ab84",
+    email: "rodriguez_steffanie@osinski-mante.io",
+    first_name: "Steffanie",
+    last_name: "Rodriguez",
+  },
+]
