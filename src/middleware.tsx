31dd16bd73fec55f1980a407f4ec55e9932fdba6
Jordian Renaldi <jrenaldi.2021@scis.smu.edu.sg>
2023-11-13 02:27:37 +0700

    Update middleware.tsx

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,14 +1,24 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { getToken } from 'next-auth/jwt'
+import { NextResponse, NextRequest } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+  const { pathname } = req.nextUrl;
 
-  if (!token && req.nextUrl.pathname !== '/login') {
-    const url = req.nextUrl.clone()
-    url.pathname = '/login'
-    return NextResponse.redirect(url)
+  // Allow if it's a request for the login page or next-auth session & provider fetching
+  if (pathname.includes('/login') || pathname.includes('/api/auth')) {
+    return NextResponse.next();
   }
 
-  return NextResponse.next()
+  // If the token exists, allow the request
+  const token = await getToken({ req });
+  if (token) {
+    return NextResponse.next();
+  }
+
+  // If no token, redirect to the login page
+  const url = req.nextUrl.clone();
+  url.pathname = '/login';
+  return NextResponse.redirect(url);
 }
+
+export const config = { matcher: "/((?!.*\\.).*)" };
